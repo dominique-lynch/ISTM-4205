da41718002ff13490d345f94bb19de791e6ffa4c
kearatoop <149437750+kearatoop@users.noreply.github.com>
2023-11-03 22:11:42 -0400

    Add files via upload

--- project.js
+++ project.js
@@ -3,17 +3,16 @@
 */
 
 /* JavaScript for Keara's Code Start */
-
-/* The purpose of these functions is the change the color of the required inputs
-from red to yellow after the user has clicked on the box to show all of the
-required boxes have been filled */
-function chBackcolorName() {
-    document.getElementById("name").style.backgroundColor = "#EEDF81";
-}
-function chBackcolorEmail() {
-    document.getElementById("email").style.backgroundColor = "#EEDF81";
-}
-function chBackcolorPass() {
-    document.getElementById("pass").style.backgroundColor = "#EEDF81";
-}
+    /* The purpose of these functions is the change the color of the required inputs
+    from red to yellow after the user has clicked on the box to show all of the
+    required boxes have been filled */
+    function chBackcolorName() {
+        document.getElementById("name").style.backgroundColor = "#EEDF81";
+    }
+    function chBackcolorEmail() {
+        document.getElementById("email").style.backgroundColor = "#EEDF81";
+    }
+    function chBackcolorPass() {
+        document.getElementById("pass").style.backgroundColor = "#EEDF81";
+    }
 /* JavaScript for Keara's Code End*/
