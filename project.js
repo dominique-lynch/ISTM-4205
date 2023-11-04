a020cfed14f38a80eae3d4984878a3627eae04a8
kearatoop <149437750+kearatoop@users.noreply.github.com>
2023-11-03 22:08:59 -0400

    Add files via upload

---
+++ project.js
@@ -0,0 +1,19 @@
+/* 
+    Group Members: Keara Toop, Catherine Perdomo, Alice Alston, Dom Lynch
+*/
+
+/* JavaScript for Keara's Code Start */
+
+/* The purpose of these functions is the change the color of the required inputs
+from red to yellow after the user has clicked on the box to show all of the
+required boxes have been filled */
+function chBackcolorName() {
+    document.getElementById("name").style.backgroundColor = "#EEDF81";
+}
+function chBackcolorEmail() {
+    document.getElementById("email").style.backgroundColor = "#EEDF81";
+}
+function chBackcolorPass() {
+    document.getElementById("pass").style.backgroundColor = "#EEDF81";
+}
+/* JavaScript for Keara's Code End*/
